1e1a8b9ca2d70ecbaf9ae72e03f8730710607a4b
vinzbarbuto <vinz.barbuto@gmail.com>
2026-03-10 02:09:56 +0100

    feat: Configure Next.js for GitHub Pages deployment by enabling static export, unoptimized images, and a base path.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Optional, but helps ensure the build behaves correctly for GH Pages
+  images: {
+    unoptimized: true, // MUST be true for GitHub Pages since it doesn't support Next.js image optimization
+  },
+  basePath: "/vincenzo-barbuto.github.io", // Required for GitHub Pages project site
 };
 
 export default nextConfig;
